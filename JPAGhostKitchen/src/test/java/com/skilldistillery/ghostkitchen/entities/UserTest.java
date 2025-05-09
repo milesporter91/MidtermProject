05489ed12cd9770926a228f682d279511cf4fe77
Michel <michelmukua@Ryan-s-S20.localdomain>
2025-05-09 11:24:43 -0600

    committed hiberbnate

--- JPAGhostKitchen/src/test/java/com/skilldistillery/ghostkitchen/entities/UserTest.java
+++ JPAGhostKitchen/src/test/java/com/skilldistillery/ghostkitchen/entities/UserTest.java
@@ -2,33 +2,52 @@ package com.skilldistillery.ghostkitchen.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.beans.PersistenceDelegate;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAGhostKitchen");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class,1);
 	}
+				
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
+
 	}
 
 }
